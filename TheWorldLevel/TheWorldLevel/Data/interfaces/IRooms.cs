a82818d3afca1d2b636b03f8a3408e5092e80437
Oleg_T <oleg.terentev123456789@outlook.com>
2020-05-22 14:01:28 +0300

    Create IRooms

---
+++ TheWorldLevel/TheWorldLevel/Data/interfaces/IRooms.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TheWorldLevel.Models;
+
+namespace TheWorldLevel.Data.interfaces
+{
+    interface IRooms
+    {
+        IEnumerable<Room> Rooms { get; }
+        Room GetRoom(int carId);
+    }
+}

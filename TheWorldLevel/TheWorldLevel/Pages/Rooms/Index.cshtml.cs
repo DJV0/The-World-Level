423c4d029fd78923c622d871a2493711ac8090ab
Oleg_T <oleg.terentev123456789@outlook.com>
2020-05-23 16:24:05 +0300

    Add photo to Room Page

--- TheWorldLevel/TheWorldLevel/Pages/Rooms/Index.cshtml.cs
+++ TheWorldLevel/TheWorldLevel/Pages/Rooms/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -14,17 +15,14 @@ namespace TheWorldLevel.Pages.Rooms
 {
     public class IndexModel : PageModel
     {
-        //private readonly TheWorldLevel.Data.ApplicationDbContext _context;
         private readonly IRooms _rooms;
 
         public IndexModel(IRooms rooms)
         {
-            //_context = context;
             _rooms = rooms;
         }
 
         public IList<Room> Room { get;set; }
-
         public void OnGet()
         {
             Room = _rooms.Rooms.ToList();
